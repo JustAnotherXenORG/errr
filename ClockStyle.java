09a5ffc702132906d373587102a7caa3fa4e05ac
xenxynon <flynryder427@gmail.com>
2024-03-15 16:55:39 +0530

    Update ClockStyle.java

--- ClockStyle.java
+++ ClockStyle.java
@@ -14,29 +14,6 @@ import com.android.settings.R;
 
 public class ClockStyle extends RelativeLayout {
 
-    private static final int[] CLOCK_VIEW_IDS = {
-            R.id.keyguard_clock_style_default,
-            R.id.keyguard_clock_style_oos,
-            R.id.keyguard_clock_style_ios,
-            R.id.keyguard_clock_style_cos,
-            R.id.keyguard_clock_style_custom,
-            R.id.keyguard_clock_style_custom1,
-            R.id.keyguard_clock_style_custom2,
-            R.id.keyguard_clock_style_custom3,
-            R.id.keyguard_clock_style_miui,
-            R.id.keyguard_clock_style_ide,
-            R.id.keyguard_clock_style_lottie,
-            R.id.keyguard_clock_style_lottie2,
-            R.id.keyguard_clock_style_fluid,
-            R.id.keyguard_clock_style_hyper,
-            R.id.keyguard_clock_style_dual,
-            R.id.keyguard_clock_style_stylish,
-            R.id.keyguard_clock_style_sidebar,
-            R.id.keyguard_clock_style_minimal,
-            R.id.keyguard_clock_style_minimal2,
-            R.id.keyguard_clock_style_minimal3
-    };
-
     private static final int DEFAULT_STYLE = 0; //Disabled
     private static final String CLOCK_STYLE_KEY = "clock_style";
     private static final Uri CLOCK_STYLE_URI = Settings.System.getUriFor(CLOCK_STYLE_KEY);
@@ -54,13 +31,20 @@ public class ClockStyle extends RelativeLayout {
     @Override
     protected void onFinishInflate() {
         super.onFinishInflate();
-        clockViews = new SparseArray<>(CLOCK_VIEW_IDS.length);
-        for (int i = 0; i < CLOCK_VIEW_IDS.length; i++) {
-            View clockView = findViewById(CLOCK_VIEW_IDS[i]);
-            clockViews.put(i, clockView);
-        }
+        findClockViews();
         new MyContentObserver(new Handler()).observe();
         updateClockView();
+    }
+
+    private void findClockViews() {
+        clockViews = new SparseArray<>();
+        int childCount = getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            View child = getChildAt(i);
+            if (child.getTag() != null && child.getTag().equals("clock_view")) {
+                clockViews.put(clockViews.size(), child);
+            }
+        }
     }
 
     private void updateClockView() {
